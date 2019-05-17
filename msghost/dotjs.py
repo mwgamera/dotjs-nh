e248288942401fed8a08130d225646088d6ab0e6
klg <mwgamera@gmail.com>
2019-05-17 12:21:07 +0200

    Initial import

---
+++ msghost/dotjs.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import os
+import sys
+import struct
+import json
+
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
+    str = unicode
+
+stdin = os.fdopen(sys.stdin.fileno(), 'rb', 0)
+stdout = os.fdopen(sys.stdout.fileno(), 'wb', 0)
+
+def dotjs(host, ext='.js', path=None):
+    if path is None:
+        path = os.path.expanduser(os.path.join('~', ext))
+    host = host.rstrip('.')
+    files = []
+    while True:
+        files.append(host)
+        try:
+            _, host = host.split('.', 1)
+        except ValueError:
+            break
+    files.append('default')
+    data = []
+    for fn in files[::-1]:
+        try:
+            if os.path.sep in fn:
+                continue
+            with open(os.path.join(path, fn+ext)) as f:
+                data.append(f.read())
+        except IOError:
+            pass
+    return data
+
+while True:
+    tag = stdin.read(4)
+    if not tag:
+        break
+    length = struct.unpack('=L', tag)[0]
+    req = json.loads(stdin.read(length).decode('UTF-8'))
+    res = []
+    try:
+        req = urlparse(req).hostname
+        if req:
+            res = [
+                dotjs(req, '.js'),
+                dotjs(req, '.css'),
+            ]
+    except Exception as e:
+        sys.stderr.write(str(e)+'\n')
+    res = json.dumps(res).encode('UTF-8')
+    stdout.write(struct.pack('=L', len(res)) + res)
+
---
+++ webext/main.js
@@ -0,0 +1,19 @@
+"use strict"
+
+let queue = []
+let port = chrome.runtime.connectNative("klg.dotjs_nh")
+port.onMessage.addListener(msg => queue.shift()(msg))
+
+chrome.webNavigation.onCommitted.addListener(details => {
+  port.postMessage(details.url)
+  queue.push(msg => {
+    if (msg[1]) msg[1].forEach(code =>
+      chrome.tabs.insertCSS(details.tabId, {
+        code, frameId: details.frameId
+      }))
+    if (msg[0]) msg[0].forEach(code =>
+      chrome.tabs.executeScript(details.tabId, {
+        code, frameId: details.frameId
+      }))
+  })
+})
